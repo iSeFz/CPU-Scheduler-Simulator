ab4e584229c6e4d011db39b723588c70ffea245e
iSeFz <seifyahia@gmail.com>
2023-12-09 23:40:43 +0200

    Modified attribute names

--- Process.java
+++ Process.java
@@ -2,14 +2,14 @@ public class Process {
     private String name;
     private int arrivalTime;
     private int burstTime;
-    private int priority;
+    private int priorityNumber;
 
     // Constructor to initialize or create new Process
-    public Process(String pName, int pArrivalTime, int pBurstTime, int pPriority) {
-        name = pName;
-        arrivalTime = pArrivalTime;
-        burstTime = pBurstTime;
-        priority = pPriority;
+    public Process(String name, int arrivalTime, int burstTime, int priorityNumber) {
+        this.name = name;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.priorityNumber = priorityNumber;
     }
 
     // Getters for process attributes
@@ -19,7 +19,7 @@ public class Process {
 
     public int getBurstTime() { return burstTime; }
 
-    public int getPriority() { return priority; }
+    public int getPriority() { return priorityNumber; }
 
     // Setters for process attributes
     public void setName(String name) { this.name = name; }
@@ -28,5 +28,5 @@ public class Process {
 
     public void setBurstTime(int burstTime) { this.burstTime = burstTime; }
 
-    public void setPriority(int priority) { this.priority = priority; }
+    public void setPriority(int priorityNumber) { this.priorityNumber = priorityNumber; }
 }
