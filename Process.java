5331ef325812fc7866b28b02b0979d1be2b5e995
Youssef18118 <101882887+Youssef18118@users.noreply.github.com>
2023-12-10 20:50:42 +0200

    Update Process

--- Process.java
+++ Process.java
@@ -2,7 +2,13 @@ public class Process {
     private String name;
     private int arrivalTime;
     private int burstTime;
-    private int priorityNumber;
+    private int priorityNumber;// for priority and AG scheduling.
+    int WaitingTime;
+    int TurnAroundTime;
+    int CompletionTime;
+    int RemainingTime; // for SJF and AG scheduling.
+    int Quantum; // for AG scheduling.
+    int AGFactor; // for AG scheduling.
 
     // Constructor to initialize or create new Process
     public Process(String name, int arrivalTime, int burstTime, int priorityNumber) {
@@ -13,20 +19,90 @@ public class Process {
     }
 
     // Getters for process attributes
-    public String getName() { return name; }
+    public String getName() {
+        return name;
+    }
 
-    public int getArrivalTime() { return arrivalTime; }
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
 
-    public int getBurstTime() { return burstTime; }
+    public int getBurstTime() {
+        return burstTime;
+    }
 
-    public int getPriority() { return priorityNumber; }
+    public int getPriority() {
+        return priorityNumber;
+    }
 
     // Setters for process attributes
-    public void setName(String name) { this.name = name; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public void setArrivalTime(int arrivalTime) { this.arrivalTime = arrivalTime; }
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
 
-    public void setBurstTime(int burstTime) { this.burstTime = burstTime; }
+    public void setBurstTime(int burstTime) {
+        this.burstTime = burstTime;
+    }
 
-    public void setPriority(int priorityNumber) { this.priorityNumber = priorityNumber; }
+    public void setPriority(int priorityNumber) {
+        this.priorityNumber = priorityNumber;
+    }
+
+    // Setter and Getter for WaitingTime
+    public int getWaitingTime() {
+        return WaitingTime;
+    }
+
+    public void setWaitingTime(int waitingTime) {
+        WaitingTime = waitingTime;
+    }
+
+    // Setter and Getter for TurnAroundTime
+    public int getTurnAroundTime() {
+        return TurnAroundTime;
+    }
+
+    public void setTurnAroundTime(int turnAroundTime) {
+        TurnAroundTime = turnAroundTime;
+    }
+
+    // Setter and Getter for CompletionTime
+    public int getCompletionTime() {
+        return CompletionTime;
+    }
+
+    public void setCompletionTime(int completionTime) {
+        CompletionTime = completionTime;
+    }
+
+    // Setter and Getter for RemainingTime
+    public int getRemainingTime() {
+        return RemainingTime;
+    }
+
+    public void setRemainingTime(int remainingTime) {
+        RemainingTime = remainingTime;
+    }
+
+    // Setter and Getter for Quantum
+    public int getQuantum() {
+        return Quantum;
+    }
+
+    public void setQuantum(int quantum) {
+        Quantum = quantum;
+    }
+
+    // Setter and Getter for AGFactor
+    public int getAGFactor() {
+        return AGFactor;
+    }
+
+    public void setAGFactor(int agFactor) {
+        AGFactor = agFactor;
+    }
 }
