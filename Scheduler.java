0b01fd44581c79ce78e5ee311a8a9ab8408ccfce
Youssef18118 <101882887+Youssef18118@users.noreply.github.com>
2023-12-10 20:51:29 +0200

    Update Scheduler

--- Scheduler.java
+++ Scheduler.java
@@ -7,7 +7,7 @@ public abstract class Scheduler {
 
     // Return the waiting time of the processes
     public abstract List<Integer> waitingTime();
-    
+
     // Return the turnaround time of the processes
     public abstract List<Integer> turnaroundTime();
 
@@ -16,32 +16,32 @@ public abstract class Scheduler {
 
     // Return the average turnaround time of the processes
     public abstract double avgTurnaroundTime();
-    
+
     // Main function to run the scheduler
-    public void startScheduler(Scheduler scheduler) {
+    public void startScheduler() {
         // Simple counter to make printing more readable
         Integer counter = 1;
 
         // Print the execution order of the processes
         System.out.println("\tExecution Order Of Processes");
-        for (Process p : scheduler.executionOrder())
+        for (Process p : this.executionOrder())
             System.out.println(counter++ + ". " + p.getName());
-        
+
         counter = 1; // Reset the counter
         // Print the waiting time of the processes
         System.out.println("\tWaiting Time Of Processes");
-        for (Integer time : scheduler.waitingTime())
+        for (Integer time : this.waitingTime())
             System.out.println("Process #" + counter++ + ": " + time);
-        
+
         counter = 1; // Reset the counter
         // Print the turnaround time of the processes
         System.out.println("\tTurnaround Time Of Processes");
-        for (Integer time : scheduler.turnaroundTime())
+        for (Integer time : this.turnaroundTime())
             System.out.println("Process #" + counter++ + ": " + time);
 
         // Print the average waiting time of the processes
-        System.out.println("Average Waiting Time: " + scheduler.avgWaitingTime());
+        System.out.println("Average Waiting Time: " + this.avgWaitingTime());
         // Print the average turnaround time of the processes
-        System.out.println("Average Turnaround Time: " + scheduler.avgTurnaroundTime());
+        System.out.println("Average Turnaround Time: " + this.avgTurnaroundTime());
     }
 }
