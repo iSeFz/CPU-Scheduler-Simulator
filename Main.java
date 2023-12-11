0c3d8f92552e6214f8c8d105691fa2c56a83f90f
iSeFz <seifyahia@gmail.com>
2023-12-11 19:11:35 +0200

    Updated the Main class with the needed input Added input of required attributes & updated the process constructor

--- Main.java
+++ Main.java
@@ -1,11 +1,47 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 // Main class to run the program
 public class Main {
     public static void main(String[] args) {
-        // Create a list of processes
-        ArrayList<Process> processes = new ArrayList<>();
-        // Create the processes
+        System.out.println("\tWelcome to our CPU Scheduler Simulator!");
+        // Get the required input from the user at the start of the program
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number of processes? ");
+        int nProcesses = scanner.nextInt();
+
+        System.out.print("Context switching time? ");
+        int contextSwitchingTime = scanner.nextInt();
+
+        System.out.print("Round robin quantum time? ");
+        int quantumTime = scanner.nextInt();
+
+        scanner.nextLine(); // Clean the input stream
+
+        // Create a list with the entered number of processes
+        ArrayList<Process> processList = new ArrayList<>(nProcesses);
+        // Get each process data from the user
+        for (int i = 0; i < nProcesses; ++i) {
+            System.out.println("\tProcess #" + (i + 1) + " Data");
+            System.out.print("Enter name: ");
+            String pName = scanner.nextLine();
+            System.out.print("Enter arrival time: ");
+            int pArrivalTime = scanner.nextInt();
+            System.out.print("Enter burst time: ");
+            int pBurstTime = scanner.nextInt();
+            System.out.print("Enter priority number: ");
+            int pPriorityNumber = scanner.nextInt();
+            scanner.nextLine(); // Clean the input stream
+
+            // Create the process object with the entered data
+            Process newProcess = new Process(pName, pArrivalTime, pBurstTime);
+            newProcess.setQuantum(quantumTime);
+            newProcess.setPriority(pPriorityNumber);
+            processList.add(newProcess);
+        }
+        scanner.close();
+
+        /* // For Testing
         Process p1 = new Process("P1", 0, 4, 3);
         Process p2 = new Process("P2", 1, 3, 2);
         Process p3 = new Process("P3", 1, 5, 1);
@@ -17,5 +53,6 @@ public class Main {
         PriorityScheduler priority = new PriorityScheduler(processes);
         priority.startScheduler();
         
+        */
     }
 }
