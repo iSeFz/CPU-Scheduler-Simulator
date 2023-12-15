c7847dff4f57d73847e58993ddfb23ae516207e2
iSeFz <seifyahia@gmail.com>
2023-12-15 21:00:03 +0200

    Updated the main class & added SRTF test case

--- Main.java
+++ Main.java
@@ -51,16 +51,14 @@ public class Main {
         int choice = scanner.nextInt();
         if (choice == 1) {
             System.out.println("\tMaintenance in progress!");
-            ShortestJobScheduler SJF = new ShortestJobScheduler(processList,
-                    contextSwitchingTime);
+            ShortestJobScheduler SJF = new ShortestJobScheduler(processList, contextSwitchingTime);
             SJF.startScheduler();
         } else if (choice == 2) {
             System.out.println("\tMaintenance in progress!");
             SRTFScheduler SRTF = new SRTFScheduler(processList);
             SRTF.startScheduler();
         } else if (choice == 3) {
-            PriorityScheduler priority = new PriorityScheduler(processList,
-                    contextSwitchingTime);
+            PriorityScheduler priority = new PriorityScheduler(processList);
             priority.startScheduler();
         } else if (choice == 4) {
             AGScheduler AG = new AGScheduler(processList);
@@ -69,56 +67,65 @@ public class Main {
         scanner.close();
         System.out.println("\tThanks for using our CPU Scheduler Simulator!");
 
-        /*
-         * // Priority Test Case
-         * Process p1 = new Process("P1", 0, 4);
-         * p1.setPriority(3);
-         * Process p2 = new Process("P2", 2, 1);
-         * p2.setPriority(2);
-         * Process p3 = new Process("P3", 4, 2);
-         * p3.setPriority(4);
-         * Process p4 = new Process("P4", 6, 1);
-         * p4.setPriority(1);
-         * Process p5 = new Process("P5", 12, 5);
-         * p5.setPriority(5);
-         * // Add the processes to the list
-         * ArrayList<Process> processes = new ArrayList<>();
-         * processes.add(p1);
-         * processes.add(p2);
-         * processes.add(p3);
-         * processes.add(p4);
-         * processes.add(p5);
-         * // Create a scheduler for the processes
-         * PriorityScheduler priority = new PriorityScheduler(processes, 2);
-         * priority.startScheduler();
-         */
+        /* // SRTF Test Case
+        Process p1 = new Process("P1", 0, 8);
+        Process p2 = new Process("P2", 1, 4);
+        Process p3 = new Process("P3", 2, 9);
+        Process p4 = new Process("P4", 3, 5);
+        // Add the processes to the list
+        List<Process> processes = new ArrayList<>();
+        processes.add(p1);
+        processes.add(p2);
+        processes.add(p3);
+        processes.add(p4);
+        // Create a scheduler for the processes
+        SRTFScheduler srtf = new SRTFScheduler(processes);
+        srtf.startScheduler(); */
+        
+        /* // Priority Test Case
+        Process p1 = new Process("P1", 0, 17);
+        p1.setPriority(4);
+        Process p2 = new Process("P2", 3, 6);
+        p2.setPriority(9);
+        Process p3 = new Process("P3", 4, 10);
+        p3.setPriority(2);
+        Process p4 = new Process("P4", 29, 4);
+        p4.setPriority(1);
+        // Add the processes to the list
+        ArrayList<Process> processes = new ArrayList<>();
+        processes.add(p1);
+        processes.add(p2);
+        processes.add(p3);
+        processes.add(p4);
+        // Create a scheduler for the processes
+        PriorityScheduler priority = new PriorityScheduler(processes);
+        priority.startScheduler(); */
 
-        // AG Assignment Sheet Test Case
-        // Process p1 = new Process("P1", 0, 17);
-        // p1.setPriority(4);
-        // p1.setQuantum(4);
-        // p1.setAGFactor(20);
-        // Process p2 = new Process("P2", 3, 6);
-        // p2.setPriority(9);
-        // p2.setQuantum(4);
-        // p2.setAGFactor(17);
-        // Process p3 = new Process("P3", 4, 10);
-        // p3.setPriority(2);
-        // p3.setQuantum(4);
-        // p3.setAGFactor(16);
-        // Process p4 = new Process("P4", 29, 4);
-        // p4.setPriority(8);
-        // p4.setQuantum(4);
-        // p4.setAGFactor(43);
-        // // Add the processes to the list
-        // List<Process> processes = new ArrayList<>();
-        // processes.add(p1);
-        // processes.add(p2);
-        // processes.add(p3);
-        // processes.add(p4);
-        // // Create a scheduler for the processes
-        // AGScheduler ag = new AGScheduler(processes);
-        // ag.startScheduler();
-        // }
+        /* // AG Assignment Sheet Test Case
+        Process p1 = new Process("P1", 0, 17);
+        p1.setPriority(4);
+        p1.setQuantum(4);
+        p1.setAGFactor(20);
+        Process p2 = new Process("P2", 3, 6);
+        p2.setPriority(9);
+        p2.setQuantum(4);
+        p2.setAGFactor(17);
+        Process p3 = new Process("P3", 4, 10);
+        p3.setPriority(2);
+        p3.setQuantum(4);
+        p3.setAGFactor(16);
+        Process p4 = new Process("P4", 29, 4);
+        p4.setPriority(8);
+        p4.setQuantum(4);
+        p4.setAGFactor(43);
+        // Add the processes to the list
+        List<Process> processes = new ArrayList<>();
+        processes.add(p1);
+        processes.add(p2);
+        processes.add(p3);
+        processes.add(p4);
+        // Create a scheduler for the processes
+        AGScheduler ag = new AGScheduler(processes);
+        ag.startScheduler(); */
     }
 }
